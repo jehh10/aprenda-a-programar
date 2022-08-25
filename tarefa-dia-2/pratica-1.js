c0764f42726b7ee231eb472c88c6a977736cce98
jehh10 <jessicamoscardi@yahoo.com.br>
2022-08-24 23:49:45 -0300

    dados

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,30 @@
-let Seu nome = Jessica
-let Seu email = jessicamoscardi@yahoo.com.br
-let Sua profissão = Estudante
-let Sua idade = 29
+let nome = ''Jessica''
+
+
+
+
+let email = ''jessicamoscardi@yahoo.com.br''
+
+
+
+
+
+
+let profissão = ''Estudante''
+
+
+
+
+
+let idade = ''29''
+
+
+
+
+
+
 let O lugar dos seus sonhos = Nova Zelandia 
+console.log(nome)
+console.log(email)
+console.log(profissão)
+console.log(idade)
